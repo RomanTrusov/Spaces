f1450004103a3aafecd8d420ac1e1e16a2d8e280
RomanTrusov <kreezzzfaqer@gmail.com>
2024-03-20 16:41:10 +0300

    Fixed climbing

--- Assets/Scripts/Player/Climb.cs
+++ Assets/Scripts/Player/Climb.cs
@@ -29,9 +29,11 @@ public class Climb : MonoBehaviour
         //if player not grounded
         if(!playerMovement.grounded)
         {
-            //if bottom + and top -
-            if (Physics.Raycast(playerRigidBody.position + new Vector3(0f,-0.5f,0f), orientation.forward, out rayBottom, 1f, whatIsClimable) &&
-                !Physics.Raycast(playerRigidBody.position + new Vector3(0f, 1.5f, 0f), orientation.forward, out rayTop, 2f, whatIsClimable))
+
+            //=== V2.0 Check if climb is needed by two Raycast Hit Vector anngles
+            if (Physics.Raycast(playerRigidBody.position + new Vector3(0f, -0.5f, 0f), orientation.forward, out rayBottom, 1f, whatIsClimable) &&
+                Physics.Raycast(playerRigidBody.position + new Vector3(0f, 0.5f, 0f) + orientation.forward, -orientation.up, out rayTop, 1f, whatIsClimable) && 
+                Vector3.Angle(orientation.forward, rayTop.normal) <= 110) // to avoid climbing while jumping on upward stairs
             {
                 StartClimb();
             }
@@ -41,11 +43,11 @@ public class Climb : MonoBehaviour
     }
 
     //====================
-    private void StartClimb() //Climbing V2
+    private void StartClimb() //Climbing V2 - without stopping player while and after climbing
     {
         if(climbCdTimer <= 0)
         {
-            // activate climbing
+            // activate climbing: no collision
             playerMovement.climbing = true;
             // zero to Y current velocity
             playerRigidBody.velocity = Vector3.Scale(playerRigidBody.velocity, new Vector3(0f,0f,0f));
@@ -56,24 +58,6 @@ public class Climb : MonoBehaviour
         }
         
     }
-
-    //====================
-    /*private void StartClimb() //CLimbing via adding force to player's rigid body
-    {
-        if(climbCdTimer <= 0)
-        {
-            // activate climbing
-            playerMovement.climbing = true;
-            // zero to current velocity
-            playerRigidBody.velocity = Vector3.zero;
-            // add velocity to climb
-            playerRigidBody.AddForce(orientation.up * climbForce * 2f + orientation.forward * climbForce * 0.7f, ForceMode.Impulse);
-            // stop climb after delay
-            Invoke(nameof(EndClimb), climbCd);
-        }
-        
-    }*/
-
 
     //====================
     private void EndClimb()
